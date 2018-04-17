d9c745574a86fe4af0c5e34c5a2a82bdf6138725
Pantea Habibi <phabib4@uic.edu>
2018-04-17 17:39:45 -0500

    final

--- logging.js
+++ logging.js
@@ -82,7 +82,7 @@ if (ENABLE_NETWORK_LOGGING) {
 
 function sendNetworkLog(uid, time, name, infoObj) {
     var formid = "e/1FAIpQLSeQKTQ9Ftrvm-wOO85Zz6QWB3aYYwzCbt9GQVf6BuQP1GNq7w";
-    var data = {"entry.232449398": uid, "entry.2000759014": time, "entry.942551297": name, "entry.1754656750": JSON.stringify(infoObj["cursorType"]), "entry.1912923506": JSON.stringify(infoObj["Block"]) , "entry.1891194768": JSON.stringify(infoObj["Trial"]) , "entry.5712609": JSON.stringify(infoObj["Target"]) , "entry.1982079988": JSON.stringify(infoObj["Amplitude"]), "entry.475157168": JSON.stringify(infoObj["R"]), "entry.1806843383": JSON.stringify(infoObj["D"]), "entry.114811808": JSON.stringify(infoObj["time"]), "entry.1053423942": JSON.stringify(infoObj["errors"])};
+    var data = {"entry.232449398": uid, "entry.2000759014": time, "entry.942551297": name, "entry.1754656750": JSON.stringify(infoObj["cursorType"]), "entry.1912923506": JSON.stringify(infoObj["Block"]) , "entry.1891194768": JSON.stringify(infoObj["Trial"]) , "entry.5712609": JSON.stringify(infoObj["Target"]) , "entry.1982079988": JSON.stringify(infoObj["Amplitude"]), "entry.63261001": JSON.stringify(infoObj["width"]), "entry.475157168": JSON.stringify(infoObj["R"]), "entry.1806843383": JSON.stringify(infoObj["D"]), "entry.114811808": JSON.stringify(infoObj["time"]), "entry.1053423942": JSON.stringify(infoObj["errors"])};
     var params = [];
     for (key in data) {
         params.push(key + "=" + encodeURIComponent(data[key]));
